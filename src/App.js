3179edf381ca0c11206ee8563c02e614bbd3b183
Sanni John <blovedjohn50@gmail.com>
2024-06-07 06:55:37 +0100

    tenth commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css'; 
+import AllCards from './DataCard/AllCard';
+
+
+function App() {
+  return (
+    <div className="App">
+<AllCards/>
+
+    </div>
+  );
+}
+
+export default App;
